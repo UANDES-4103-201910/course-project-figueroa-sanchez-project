d9c63882a1efb22c6fb2958ee14b4027ba052d3b
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-05-08 19:15:04 -0400

    Footer changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
 Rails.application.routes.draw do
+  get 'registration/index'
   get 'change_password/index'
   get 'recover_password/index'
   get 'profile_view/index'
   get 'searcher/index'
   get 'login/index'
   root :to => 'home#home'
-  resources :admin_black_lists
-  resources :user_roles
-  resources :roles
   resources :black_lists
   resources :admin_locations
   resources :reports
   resources :comments
-  resources :validations
   resources :shared_posts
   resources :dumpsters
-  resources :attachments
-  resources :profile_photos
-  resources :profile_locations
   resources :profiles
-  resources :user_passwords
   resources :users
   resources :login
   resources :searcher
