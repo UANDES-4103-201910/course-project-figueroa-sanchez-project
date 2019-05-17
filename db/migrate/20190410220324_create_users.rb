4a2800c3fff39e0be633852a89edefe7f9f43f9b
mpsanchez2@miuandes.cl <mapisanchezg@gmail.com>
2019-05-17 19:57:25 -0400

    fix(signin):auth with google workinggit add .git add .

--- db/migrate/20190410220324_create_users.rb
+++ db/migrate/20190410220324_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :uid
+      t.string :provider
       t.boolean :is_active
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -141,14 +141,6 @@ ActiveRecord::Schema.define(version: 2019_05_17_215720) do
     t.index ["user_id"], name: "index_shared_posts_on_user_id"
   end
 
-  create_table "user_passwords", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_passwords_on_user_id"
-  end
-
   create_table "user_roles", force: :cascade do |t|
     t.integer "role_id"
     t.integer "user_id"
@@ -159,6 +151,9 @@ ActiveRecord::Schema.define(version: 2019_05_17_215720) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "uid"
+    t.string "provider"
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -167,8 +162,22 @@ ActiveRecord::Schema.define(version: 2019_05_17_215720) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   create_table "validations", force: :cascade do |t|
