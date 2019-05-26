656af940373e8440511dc004bc97e84fe9676ba3
mpsanchez2@miuandes.cl <mapisanchezg@gmail.com>
2019-05-26 18:42:08 -0400

    feat(search):create the searches model and the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'destroy/searcher'
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}
   get 'admin_list/', to:'admin_list#index'
   get 'super_admin_home/', to: 'super_admin_home#index'
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   get 'report_post/:id', to: 'report_post#index'
   get 'post_view/:id', to: 'post_view#index'
   get 'registration/', to: 'registration#index'
+  get 'searches/', to: 'searches#index'
   get 'change_password/index'
   get 'recover_password/index'
   get 'profile_view/index'
@@ -20,7 +22,7 @@ Rails.application.routes.draw do
   resources :dumpsters
   resources :profiles
   resources :login
-  resources :searcher
+  resources :searches
   resources :profile_view
   resources :post_view
   defaults format: :json do
--- test/controllers/searcher_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SearcherControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get searcher_index_url
-    assert_response :success
-  end
-
-end
