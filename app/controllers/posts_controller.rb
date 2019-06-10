3a6e4e8b126a83de5876eee4eef4df7527247a49
ignaciofigueroa <ijfigueroa@gmail.com>
2019-06-09 23:34:03 -0400

    Posts now can have locations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,11 +31,15 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     parameters = post_params
-    parameters["user"] = current_user
-    @post = Post.new(parameters)
+    @post = Post.new(title: parameters[:title], description: parameters[:description], user_id:current_user.id)
+    latitude = post_params[:post_locations][:lat].to_f
+    longitude = post_params[:post_locations][:lng].to_f
 
     respond_to do |format|
       if @post.save
+        if post_params[:post_locations][:lat] != "" and post_params[:post_locations][:lng]!= ""
+          @post_location = PostLocation.create!(latitude: latitude, longitude: longitude, post_id:@post.id)
+        end
         format.html {redirect_back(fallback_location: profile_path(@current_profile.id)); flash[:success] = 'Post created'}
       else
         format.html {redirect_back(fallback_location: profile_path(@current_profile.id)); flash[:danger] = 'Error creating post'}
@@ -48,11 +52,11 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.html {redirect_to @post, notice: 'Post was successfully updated.'}
+        format.json {render :show, status: :ok, location: @post}
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @post.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -62,39 +66,40 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head 200 }
+      format.html {redirect_to posts_url, notice: 'Post was successfully destroyed.'}
+      format.json {head 200}
     end
   end
 
   def vote_up
-    validation = Validation.create(post: Post.find_by_id($current_post) , user: current_user, vote: 1)
+    validation = Validation.create(post: Post.find_by_id($current_post), user: current_user, vote: 1)
     redirect_back(fallback_location: root_path)
   end
 
   def vote_down
-    validation = Validation.create(post: Post.find_by_id($current_post) , user: current_user, vote: -1)
+    validation = Validation.create(post: Post.find_by_id($current_post), user: current_user, vote: -1)
     redirect_back(fallback_location: root_path)
   end
 
   def follow_post
-    follow_post = FollowPost.create(post: Post.find_by_id($current_post) , user: current_user)
+    follow_post = FollowPost.create(post: Post.find_by_id($current_post), user: current_user)
     redirect_back(fallback_location: root_path)
   end
 
   def new_comment
-    comment = Comment.create(post: Post.find_by_id($current_post) , user: current_user, comment: $new_comment)
+    comment = Comment.create(post: Post.find_by_id($current_post), user: current_user, comment: $new_comment)
     redirect_back(fallback_location: root_path)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :description, images: [], files: [])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :description, images: [], files: [], post_locations: [:lat, :lng])
+  end
 end
